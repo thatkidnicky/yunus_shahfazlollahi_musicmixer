c8168b650a86f1411d692fb099de123010f21369
unknown <b_yunus@fanshaweonline.ca>
2019-01-30 19:03:09 -0500

    created html and css

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//create a pickel jar, label it has a firstName, put a string in it
+//var firstName = "Brisk";
+
+//alert("Hello " + firstName);
+
+//modern js
+
+//alert(`Hello ${firstName} !!!`);
+
+//console.log(firstName);
+
+console.log("Javascript is linked up");
+
+(() => {
+  // self invoking anonymous function (or Lambda)
+  // var => means create a variable
+  // whatever comes after the = is what the variable reads
+  var images = document.querySelectorAll('img');
+  //document is always the webpage
+  // after the dot is alway a method or a property
+
+  console.log(images);
+
+})();
